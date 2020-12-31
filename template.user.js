eb99ad81c654937068cad90f227c74b5bc8e4fd5
tmwebscript <76825522+tmwebscript@users.noreply.github.com>
2020-12-31 13:54:22 -0800

    Create template.user.js

---
+++ template.user.js
@@ -0,0 +1,15 @@
+// @name         tmwebscript
+// @description  
+// @match        http://*/*
+// @match        https://*/*
+// @require      https://tmwebscript.github.io/script/script.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @grant        GM_xmlhttpRequest
+// @grant        unsafewindow
+// @grant        window.close
+// @grant        window.focus
+// @grant        GM_openInTab
+// @grant        GM_addValueChangeListener
