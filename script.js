5e7374df57f248238bbfb2ab65520fe4c1ad4e7a
tmwebscript <76825522+tmwebscript@users.noreply.github.com>
2020-12-31 16:16:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('it works too')
+alert(test)
+url = "https://tmwebscript.github.io/script/script2.js"
+
+// setInterval(async()=>{
+	GM_xmlhttpRequest({
+               method: "GET",
+               url: url,
+               onload: data=> eval(data.responseText)
+             });
+// }, 30*1000)
