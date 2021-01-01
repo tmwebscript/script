4334218d29e4e3b0c85e9db71e44c7451a67cde2
tmwebscript <76825522+tmwebscript@users.noreply.github.com>
2020-12-31 16:44:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log(test); // variable defined in main script
-url = "https://tmwebscript.github.io/script/script2.js"
+url = "https://tmwebscript.github.io/script/script2.js" + "?nocache=" + Date.now();
 
 // setInterval(async()=>{
 	GM_xmlhttpRequest({
